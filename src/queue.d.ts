f6accc521ccc5d987fab1c6902f7482c53cbf40e
Dejan Todorovic <dejan96@mail.ru>
2021-08-06 00:35:17 +0200

    add tslint and prettier and format project

--- src/queue.d.ts
+++ src/queue.d.ts
@@ -1,17 +1,15 @@
-
-export type PriorityQueueComparator<T> = (((a: T, b: T) => boolean));
+export type PriorityQueueComparator<T> = (a: T, b: T) => boolean;
 
 interface PriorityQueueOptions<T> {
-    comparator: PriorityQueueComparator<T>;
-    initialElements?: T[];
+  comparator: PriorityQueueComparator<T>;
+  initialElements?: T[];
 }
 
-export interface  IPriorityQueue<T>{
-    size(): number;
-    isEmpty(): boolean;
-    peek() :T;
-    push(value) : void;
-    pushMany(values) : void;
-    pop() :T;
+export interface IPriorityQueue<T> {
+  size(): number;
+  isEmpty(): boolean;
+  peek(): T;
+  push(value): void;
+  pushMany(values): void;
+  pop(): T;
 }
-
--- src/queue.ts
+++ src/queue.ts
@@ -1,129 +1,131 @@
-import {IPriorityQueue,PriorityQueueOptions} from './queue.d';
+import { IPriorityQueue, PriorityQueueOptions } from './queue.d';
 
-export class PriorityQueue<T> implements IPriorityQueue<T>{
+export class PriorityQueue<T> implements IPriorityQueue<T> {
+  private heap: T[];
+  private comparator;
 
-    private heap: T[];
-    private comparator;
+  /**
+   *
+   * @param options
+   * options.comparator: function used to compare elements;
+   * options.initialElements: elements to be put in priority queue initially in O(n) time
+   */
+  constructor(options: PriorityQueueOptions<T>) {
+    this.heap = [];
+    this.comparator = options.comparator;
+    if (options.initialElements) this.buildHeap(options.initialElements);
+  }
 
-    /**
-     * 
-     * @param options 
-     * options.comparator: function used to compare elements; 
-     * options.initialElements: elements to be put in priority queue initially in O(n) time
-     */
-    constructor(options: PriorityQueueOptions<T>) {
-        this.heap = [];
-        this.comparator = options.comparator;
-        if (options.initialElements) this.buildHeap(options.initialElements)
+  /**
+   *
+   * @returns size of priority queue in O(1)
+   */
+  size() {
+    return this.heap.length;
+  }
+
+  /**
+   *
+   * @returns is priority queue empty in O(1)
+   */
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  /**
+   *
+   * @returns top of priority queue in O(1)
+   */
+  peek() {
+    return this.heap[0];
+  }
+
+  private buildHeap(array: T[]) {
+    this.heap = array;
+    for (let i = Math.floor(array.length / 2) - 1; i >= 0; i--) {
+      this.siftDown();
     }
+  }
 
-    /**
-     * 
-     * @returns size of priority queue in O(1)
-     */
-    size() {
-        return this.heap.length;
-    }
+  /**
+   *
+   * @param value element to be added to heap, adds it in O(log n) operations, n is size of heap
+   * @returns size of heap
+   */
+  push(value) {
+    this.heap.push(value);
+    this.siftUp();
+    return this.size();
+  }
 
-    /**
-     * 
-     * @returns is priority queue empty in O(1)
-     */
-    isEmpty() {
-        return this.heap.length === 0;
-    }
+  /**
+   *
+   * @param values elements to be added to heap, adds it in O(k * log n) operations, n is size of heap, k is number of elements added
+   * @returns size of heap
+   */
+  pushMany(values) {
+    values.forEach((value) => {
+      this.push(value);
+    });
+    return this.size();
+  }
 
-    /**
-     * 
-     * @returns top of priority queue in O(1)
-     */
-    peek() {
-        return this.heap[0];
+  /**
+   *
+   * @returns top of priority queue and removes it from priority queue in O(log n)
+   */
+  pop() {
+    const returnValue = this.peek();
+    const lastIndex = this.size() - 1;
+    if (lastIndex > 0) {
+      this.swap(0, lastIndex);
     }
+    this.heap.pop();
+    this.siftDown();
+    return returnValue;
+  }
 
-    private buildHeap(array: T[]) {
-        this.heap = array;
-        for (let i = Math.floor(array.length / 2) - 1; i >= 0; i--) {
-            this.siftDown()
-        }
-    }
+  private getParent(index) {
+    return ((index + 1) >>> 1) - 1;
+  }
 
-    /**
-     * 
-     * @param value element to be added to heap, adds it in O(log n) operations, n is size of heap
-     * @returns size of heap
-     */
-    push(value) {
-        this.heap.push(value);
-        this.siftUp();
-        return this.size();
-    }
+  private getLeftChild(index) {
+    return (index << 1) + 1;
+  }
 
-    /**
-     * 
-     * @param values elements to be added to heap, adds it in O(k * log n) operations, n is size of heap, k is number of elements added
-     * @returns size of heap
-     */
-    pushMany(values) {
-        values.forEach(value => {
-            this.push(value);
-        });
-        return this.size();
-    }
+  private getRightChild(index) {
+    return (index + 1) << 1;
+  }
 
-    /**
-     * 
-     * @returns top of priority queue and removes it from priority queue in O(log n)
-     */
-    pop() {
-        const returnValue = this.peek();
-        const lastIndex = this.size() - 1;
-        if (lastIndex > 0) {
-            this.swap(0, lastIndex);
-        }
-        this.heap.pop();
-        this.siftDown();
-        return returnValue;
-    }
+  private compareByIndex(i, j) {
+    return this.comparator(this.heap[i], this.heap[j]);
+  }
 
-    private getParent(index) {
-        return ((index + 1) >>> 1) - 1;
-    }
+  private swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
 
-    private getLeftChild(index) {
-        return (index << 1) + 1;
+  private siftUp() {
+    let node = this.size() - 1;
+    while (node > 0 && this.compareByIndex(node, this.getParent(node))) {
+      const parentNode = this.getParent(node);
+      this.swap(node, parentNode);
+      node = parentNode;
     }
+  }
 
-    private getRightChild(index) {
-        return (index + 1) << 1;
+  private siftDown() {
+    let node = 0;
+    while (
+      (this.getLeftChild(node) < this.size() && this.compareByIndex(this.getLeftChild(node), node)) ||
+      (this.getRightChild(node) < this.size() && this.compareByIndex(this.getRightChild(node), node))
+    ) {
+      const maxChild =
+        this.getRightChild(node) < this.size() && this.compareByIndex(this.getRightChild(node), this.getLeftChild(node))
+          ? this.getRightChild(node)
+          : this.getLeftChild(node);
+      this.swap(node, maxChild);
+      node = maxChild;
     }
-
-    private compareByIndex(i, j) {
-        return this.comparator(this.heap[i], this.heap[j]);
-    }
-
-    private swap(i, j) {
-        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
-    }
-
-    private siftUp() {
-        let node = this.size() - 1;
-        while (node > 0 && this.compareByIndex(node, this.getParent(node))) {
-            const parentNode = this.getParent(node);
-            this.swap(node, parentNode);
-            node = parentNode;
-        }
-    }
-
-    private siftDown() {
-        let node = 0;
-        while (
-            (this.getLeftChild(node) < this.size() && this.compareByIndex(this.getLeftChild(node), node)) ||
-            (this.getRightChild(node) < this.size() && this.compareByIndex(this.getRightChild(node), node))
-        ) {
-            let maxChild = (this.getRightChild(node) < this.size() && this.compareByIndex(this.getRightChild(node), this.getLeftChild(node))) ? this.getRightChild(node) : this.getLeftChild(node);
-            this.swap(node, maxChild);
-            node = maxChild;
-        }
-    }
+  }
 }
