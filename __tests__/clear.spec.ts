316860db01d8983c35e1b9a9b90c2894e89fd614
Dejan Todorovic <dejan96@mail.ru>
2021-08-09 01:09:02 +0200

    add tests for clear function

---
+++ __tests__/clear.spec.ts
@@ -0,0 +1,23 @@
+import { PriorityQueue } from "../lib/queue";
+import { defaultMaxComparator } from "./test.helper";
+
+test('initial state (created with initialElements)', () => {
+    const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator, initialElements: [2, 3, 1] });
+
+    expect(numberPriorityQueue.size()).toBe(3);
+
+    numberPriorityQueue.clear();
+
+    expect(numberPriorityQueue.size()).toBe(0);
+});
+
+test('initial state (created without initialElements)', () => {
+    const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator });
+
+    expect(numberPriorityQueue.size()).toBe(0);
+
+    numberPriorityQueue.clear();
+
+    expect(numberPriorityQueue.size()).toBe(0);
+});
+
