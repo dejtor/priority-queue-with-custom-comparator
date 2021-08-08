201b1682294acbd6d57503861cd4663b00621192
Dejan Todorovic <dejan96@mail.ru>
2021-08-09 01:06:13 +0200

    add tests for peek function

---
+++ __tests__/peek.spec.ts
@@ -0,0 +1,13 @@
+import { PriorityQueue } from "../lib/queue";
+import { defaultMaxComparator } from "./test.helper";
+
+test('initial state (created with initialElements)', () => {
+    const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator, initialElements: [2, 3, 1] });
+    expect(numberPriorityQueue.peek()).toBe(3);
+});
+
+test('initial state (created without initialElements)', () => {
+    const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator });
+    expect(numberPriorityQueue.peek()).toBeUndefined();
+});
+
