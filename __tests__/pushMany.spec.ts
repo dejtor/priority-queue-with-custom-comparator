dc18438a2a8fa3c7bcce83735703d8de0f664ab0
Dejan Todorovic <dejan96@mail.ru>
2021-08-11 23:25:29 +0200

    tests for pushMany

---
+++ __tests__/pushMany.spec.ts
@@ -0,0 +1,22 @@
+import { PriorityQueue } from '../lib/queue';
+import { defaultMaxComparator } from './test.helper';
+
+test('initial state (created without initialElements) and some added', () => {
+    const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator });
+
+    expect(numberPriorityQueue.has(5)).toBe(false);
+    expect(numberPriorityQueue.has(2)).toBe(false);
+    expect(numberPriorityQueue.has(0)).toBe(false);
+    expect(numberPriorityQueue.has(-1)).toBe(false);
+    expect(numberPriorityQueue.values().toString()).toBe([].toString());
+    expect(numberPriorityQueue.size()).toBe(0);
+
+    numberPriorityQueue.pushMany([5, 2, -1]);
+
+    expect(numberPriorityQueue.has(5)).toBe(true);
+    expect(numberPriorityQueue.has(2)).toBe(true);
+    expect(numberPriorityQueue.has(0)).toBe(false);
+    expect(numberPriorityQueue.has(-1)).toBe(true);
+    expect(numberPriorityQueue.values().toString()).toBe([5, 2, -1].toString());
+    expect(numberPriorityQueue.size()).toBe(3);
+});
