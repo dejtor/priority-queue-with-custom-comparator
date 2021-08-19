49aaf6b5719184e15538be4c0a902e1a9ef29a10
Dejan Todorovic <dejan96@mail.ru>
2021-08-19 23:20:24 +0200

    modify push function tests

--- __tests__/push.spec.ts
+++ __tests__/push.spec.ts
@@ -1,6 +1,21 @@
 import { PriorityQueue } from '../lib/queue';
 import { defaultMaxComparator } from './test.helper';
 
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+test('check if needed functions are called', () => {
+  const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator });
+  const siftUpFn = jest.spyOn(PriorityQueue.prototype as any, 'siftUp');
+  const sizeFn = jest.spyOn(numberPriorityQueue, 'size');
+
+  numberPriorityQueue.push(6);
+
+  expect(siftUpFn).toBeCalledTimes(1);
+  expect(sizeFn).toBeCalledTimes(2);
+});
+
 test('initial state (created without initialElements) and some added', () => {
   const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator });
 
@@ -8,6 +23,10 @@ test('initial state (created without initialElements) and some added', () => {
   expect(numberPriorityQueue.has(2)).toBe(false);
   expect(numberPriorityQueue.has(0)).toBe(false);
   expect(numberPriorityQueue.has(-1)).toBe(false);
+  expect(numberPriorityQueue.has(6)).toBe(false);
+  expect(numberPriorityQueue.has(-2)).toBe(false);
+  expect(numberPriorityQueue.has(-9)).toBe(false);
+  expect(numberPriorityQueue.has(58)).toBe(false);
   expect(numberPriorityQueue.values().toString()).toBe([].toString());
   expect(numberPriorityQueue.size()).toBe(0);
 
@@ -17,6 +36,12 @@ test('initial state (created without initialElements) and some added', () => {
   expect(numberPriorityQueue.has(2)).toBe(false);
   expect(numberPriorityQueue.has(0)).toBe(false);
   expect(numberPriorityQueue.has(-1)).toBe(false);
+  expect(numberPriorityQueue.has(6)).toBe(false);
+  expect(numberPriorityQueue.has(-2)).toBe(false);
+  expect(numberPriorityQueue.has(-9)).toBe(false);
+  expect(numberPriorityQueue.has(58)).toBe(false);
+  expect(numberPriorityQueue.has(185)).toBe(false);
+  expect(numberPriorityQueue.has(201)).toBe(false);
   expect(numberPriorityQueue.values().toString()).toBe([5].toString());
   expect(numberPriorityQueue.size()).toBe(1);
 
@@ -26,6 +51,12 @@ test('initial state (created without initialElements) and some added', () => {
   expect(numberPriorityQueue.has(2)).toBe(false);
   expect(numberPriorityQueue.has(0)).toBe(false);
   expect(numberPriorityQueue.has(-1)).toBe(true);
+  expect(numberPriorityQueue.has(6)).toBe(false);
+  expect(numberPriorityQueue.has(-2)).toBe(false);
+  expect(numberPriorityQueue.has(-9)).toBe(false);
+  expect(numberPriorityQueue.has(58)).toBe(false);
+  expect(numberPriorityQueue.has(185)).toBe(false);
+  expect(numberPriorityQueue.has(201)).toBe(false);
   expect(numberPriorityQueue.values().toString()).toBe([5, -1].toString());
   expect(numberPriorityQueue.size()).toBe(2);
 
@@ -35,6 +66,117 @@ test('initial state (created without initialElements) and some added', () => {
   expect(numberPriorityQueue.has(2)).toBe(true);
   expect(numberPriorityQueue.has(0)).toBe(false);
   expect(numberPriorityQueue.has(-1)).toBe(true);
+  expect(numberPriorityQueue.has(6)).toBe(false);
+  expect(numberPriorityQueue.has(-2)).toBe(false);
+  expect(numberPriorityQueue.has(-9)).toBe(false);
+  expect(numberPriorityQueue.has(58)).toBe(false);
+  expect(numberPriorityQueue.has(185)).toBe(false);
+  expect(numberPriorityQueue.has(201)).toBe(false);
   expect(numberPriorityQueue.values().toString()).toBe([5, -1, 2].toString());
   expect(numberPriorityQueue.size()).toBe(3);
+
+  numberPriorityQueue.push(6);
+
+  expect(numberPriorityQueue.has(5)).toBe(true);
+  expect(numberPriorityQueue.has(2)).toBe(true);
+  expect(numberPriorityQueue.has(0)).toBe(false);
+  expect(numberPriorityQueue.has(-1)).toBe(true);
+  expect(numberPriorityQueue.has(6)).toBe(true);
+  expect(numberPriorityQueue.has(-2)).toBe(false);
+  expect(numberPriorityQueue.has(-9)).toBe(false);
+  expect(numberPriorityQueue.has(58)).toBe(false);
+  expect(numberPriorityQueue.has(185)).toBe(false);
+  expect(numberPriorityQueue.has(201)).toBe(false);
+  expect(numberPriorityQueue.values().toString()).toBe([6, 5, 2, -1].toString());
+  expect(numberPriorityQueue.size()).toBe(4);
+
+  numberPriorityQueue.push(6);
+
+  expect(numberPriorityQueue.has(5)).toBe(true);
+  expect(numberPriorityQueue.has(2)).toBe(true);
+  expect(numberPriorityQueue.has(0)).toBe(false);
+  expect(numberPriorityQueue.has(-1)).toBe(true);
+  expect(numberPriorityQueue.has(6)).toBe(true);
+  expect(numberPriorityQueue.has(-2)).toBe(false);
+  expect(numberPriorityQueue.has(-9)).toBe(false);
+  expect(numberPriorityQueue.has(58)).toBe(false);
+  expect(numberPriorityQueue.has(185)).toBe(false);
+  expect(numberPriorityQueue.has(201)).toBe(false);
+  expect(numberPriorityQueue.values().toString()).toBe([6, 6, 2, -1, 5].toString());
+  expect(numberPriorityQueue.size()).toBe(5);
+
+  numberPriorityQueue.push(-2);
+
+  expect(numberPriorityQueue.has(5)).toBe(true);
+  expect(numberPriorityQueue.has(2)).toBe(true);
+  expect(numberPriorityQueue.has(0)).toBe(false);
+  expect(numberPriorityQueue.has(-1)).toBe(true);
+  expect(numberPriorityQueue.has(6)).toBe(true);
+  expect(numberPriorityQueue.has(-2)).toBe(true);
+  expect(numberPriorityQueue.has(-9)).toBe(false);
+  expect(numberPriorityQueue.has(58)).toBe(false);
+  expect(numberPriorityQueue.has(185)).toBe(false);
+  expect(numberPriorityQueue.has(201)).toBe(false);
+  expect(numberPriorityQueue.values().toString()).toBe([6, 6, 2, -1, 5, -2].toString());
+  expect(numberPriorityQueue.size()).toBe(6);
+
+  numberPriorityQueue.push(-9);
+
+  expect(numberPriorityQueue.has(5)).toBe(true);
+  expect(numberPriorityQueue.has(2)).toBe(true);
+  expect(numberPriorityQueue.has(0)).toBe(false);
+  expect(numberPriorityQueue.has(-1)).toBe(true);
+  expect(numberPriorityQueue.has(6)).toBe(true);
+  expect(numberPriorityQueue.has(-2)).toBe(true);
+  expect(numberPriorityQueue.has(-9)).toBe(true);
+  expect(numberPriorityQueue.has(58)).toBe(false);
+  expect(numberPriorityQueue.has(185)).toBe(false);
+  expect(numberPriorityQueue.has(201)).toBe(false);
+  expect(numberPriorityQueue.values().toString()).toBe([6, 6, 2, -1, 5, -2, -9].toString());
+  expect(numberPriorityQueue.size()).toBe(7);
+
+  numberPriorityQueue.push(58);
+
+  expect(numberPriorityQueue.has(5)).toBe(true);
+  expect(numberPriorityQueue.has(2)).toBe(true);
+  expect(numberPriorityQueue.has(0)).toBe(false);
+  expect(numberPriorityQueue.has(-1)).toBe(true);
+  expect(numberPriorityQueue.has(6)).toBe(true);
+  expect(numberPriorityQueue.has(-2)).toBe(true);
+  expect(numberPriorityQueue.has(-9)).toBe(true);
+  expect(numberPriorityQueue.has(58)).toBe(true);
+  expect(numberPriorityQueue.has(185)).toBe(false);
+  expect(numberPriorityQueue.has(201)).toBe(false);
+  expect(numberPriorityQueue.values().toString()).toBe([58, 6, 2, 6, 5, -2, -9, -1].toString());
+  expect(numberPriorityQueue.size()).toBe(8);
+
+  numberPriorityQueue.push(185);
+
+  expect(numberPriorityQueue.has(5)).toBe(true);
+  expect(numberPriorityQueue.has(2)).toBe(true);
+  expect(numberPriorityQueue.has(0)).toBe(false);
+  expect(numberPriorityQueue.has(-1)).toBe(true);
+  expect(numberPriorityQueue.has(6)).toBe(true);
+  expect(numberPriorityQueue.has(-2)).toBe(true);
+  expect(numberPriorityQueue.has(-9)).toBe(true);
+  expect(numberPriorityQueue.has(58)).toBe(true);
+  expect(numberPriorityQueue.has(185)).toBe(true);
+  expect(numberPriorityQueue.has(201)).toBe(false);
+  expect(numberPriorityQueue.values().toString()).toBe([185, 58, 2, 6, 5, -2, -9, -1, 6].toString());
+  expect(numberPriorityQueue.size()).toBe(9);
+
+  numberPriorityQueue.push(201);
+
+  expect(numberPriorityQueue.has(5)).toBe(true);
+  expect(numberPriorityQueue.has(2)).toBe(true);
+  expect(numberPriorityQueue.has(0)).toBe(false);
+  expect(numberPriorityQueue.has(-1)).toBe(true);
+  expect(numberPriorityQueue.has(6)).toBe(true);
+  expect(numberPriorityQueue.has(-2)).toBe(true);
+  expect(numberPriorityQueue.has(-9)).toBe(true);
+  expect(numberPriorityQueue.has(58)).toBe(true);
+  expect(numberPriorityQueue.has(185)).toBe(true);
+  expect(numberPriorityQueue.has(201)).toBe(true);
+  expect(numberPriorityQueue.values().toString()).toBe([201, 185, 2, 6, 58, -2, -9, -1, 6, 5].toString());
+  expect(numberPriorityQueue.size()).toBe(10);
 });
