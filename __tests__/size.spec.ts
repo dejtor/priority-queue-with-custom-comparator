925b9d0eb0bc4b3b58ca170a78dc62b8ccbcd805
Dejan Todorovic <dejan96@mail.ru>
2021-08-19 21:15:21 +0200

    new tests from size function

--- __tests__/size.spec.ts
+++ __tests__/size.spec.ts
@@ -1,12 +1,30 @@
 import { PriorityQueue } from '../lib/queue';
 import { defaultMaxComparator } from './test.helper';
 
-test('initial state (created with initialElements)', () => {
+test('initial state (created with initialElements) and no elements added', () => {
   const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator, initialElements: [3, 1] });
   expect(numberPriorityQueue.size()).toBe(2);
 });
 
-test('initial state (created without initialElements)', () => {
+test('initial state (created without initialElements) and no elements added', () => {
   const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator });
   expect(numberPriorityQueue.size()).toBe(0);
+});
+
+test('initial state (created with initialElements) and some elements added', () => {
+  const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator, initialElements: [3, 1] });
+  expect(numberPriorityQueue.size()).toBe(2);
+  numberPriorityQueue.push(4);
+  expect(numberPriorityQueue.size()).toBe(3);
+  numberPriorityQueue.pushMany([7, 8]);
+  expect(numberPriorityQueue.size()).toBe(5);
+});
+
+test('initial state (created without initialElements) and some elements added', () => {
+  const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator });
+  expect(numberPriorityQueue.size()).toBe(0);
+  numberPriorityQueue.push(4);
+  expect(numberPriorityQueue.size()).toBe(1);
+  numberPriorityQueue.pushMany([7, 8]);
+  expect(numberPriorityQueue.size()).toBe(3);
 });
