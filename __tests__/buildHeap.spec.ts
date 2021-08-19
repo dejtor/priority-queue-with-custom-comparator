2aa330d094d570f1b685deebd19351860065f3b3
Dejan Todorovic <dejan96@mail.ru>
2021-08-19 21:04:09 +0200

    tests for buildHeap function

---
+++ __tests__/buildHeap.spec.ts
@@ -0,0 +1,23 @@
+import { PriorityQueue } from '../lib/queue';
+import { defaultMaxComparator } from './test.helper';
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+test('initial state (created with initialElements)', () => {
+    const buildHeapFn = jest.spyOn(PriorityQueue.prototype as any, 'buildHeap');
+    const numberPriorityQueue = new PriorityQueue<number>({
+        comparator: defaultMaxComparator,
+        initialElements: [2, 6, 7],
+    });
+    expect(buildHeapFn).toBeCalledTimes(1);
+    expect(numberPriorityQueue.values().toString()).toBe([7, 6, 2].toString());
+});
+
+test('initial state (created without initialElements)', () => {
+    const buildHeapFn = jest.spyOn(PriorityQueue.prototype as any, 'buildHeap');
+    const numberPriorityQueue = new PriorityQueue<number>({ comparator: defaultMaxComparator });
+    expect(buildHeapFn).toBeCalledTimes(0);
+    expect(numberPriorityQueue.values().toString()).toBe([].toString());
+});
